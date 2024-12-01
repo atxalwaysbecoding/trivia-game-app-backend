656aec249197edecd4f10d71828f10f5a8f51989
Michael Lozano <michaellozano@Michaels-Mac-Studio.local>
2024-12-01 13:13:37 -0600

    Lozano: basic app setup

---
+++ src/main/java/com/lozano/showcase/trivia_game_app_backend/TriviaGameAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lozano.showcase.trivia_game_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaGameAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaGameAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lozano/showcase/trivia_game_app_backend/TriviaGameAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lozano.showcase.trivia_game_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaGameAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
